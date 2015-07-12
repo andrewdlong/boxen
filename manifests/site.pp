c46eceecdc572fe4a5f18f2dc3f2d979232e3882
adminandrew <adminandrew@air.local>
2015-07-12 13:50:25 -0400

    initial setup

--- manifests/site.pp
+++ manifests/site.pp
@@ -64,25 +64,103 @@ node default {
   }
 
   # node versions
-  nodejs::version { 'v0.6': }
-  nodejs::version { 'v0.8': }
-  nodejs::version { 'v0.10': }
+#  nodejs::version { 'v0.6': }
+#  nodejs::version { 'v0.8': }
+#  nodejs::version { 'v0.10': }
 
   # default ruby versions
-  ruby::version { '1.9.3': }
-  ruby::version { '2.0.0': }
-  ruby::version { '2.1.0': }
-  ruby::version { '2.1.1': }
-  ruby::version { '2.1.2': }
+#  ruby::version { '1.9.3': }
+#  ruby::version { '2.0.0': }
+#  ruby::version { '2.1.0': }
+#  ruby::version { '2.1.1': }
+#  ruby::version { '2.1.2': }
 
   # common, useful packages
   package {
     [
-      'ack',
+
       'findutils',
       'gnu-tar'
     ]:
   }
+	# Install term2
+	include iterm2::stable
+
+
+	#install 1passwod
+	include onepassword
+
+	include onepassword::chrome
+
+	# install quicksilver
+	include quicksilver
+
+	# For the latest version of v2
+	include sublime_text::v2
+	sublime_text::v2::package { 'Emmet':
+	  source => 'sergeche/emmet-sublime'
+	}
+
+	# install omnifocus
+	include omnifocus
+
+	# install evernote
+	include evernote
+
+	# install dropbox
+	include dropbox
+
+	# install thunderbird
+	include thunderbird
+
+	# install firefox
+	include firefox
+
+	# install vmware fusion
+	include vmware_fusion
+
+	# install chrome
+	include chrome
+
+	# include skitch
+	include skitch
+	
+	# include spotify
+	include spotify 
+
+	# include dash
+	include dash
+
+	# include trackballworks
+	include trackballworks
+
+	# include vlc
+	include vlc	
+
+	# install transit
+	include transmit
+
+	# install sequel_pro
+	include sequel_pro 
+
+	# install divvy
+	include divvy
+
+	# install google drive
+	include googledrive	
+
+	# install airfoil
+	include airfoil
+
+	# install xtrafinder
+	include xtrafinder
+
+	# install getsync
+	include btsync
+
+	# Install the default version of both the JDK and JRE
+	include java
+
 
   file { "${boxen::config::srcdir}/our-boxen":
     ensure => link,
